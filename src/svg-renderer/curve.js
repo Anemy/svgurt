3e0f27194ec79fdce4c4d958903b48518fef9ed9
Rhys Howell <rhysh@live.com>
2018-04-30 14:59:20 -0400

    Add curve in new format

---
+++ src/svg-renderer/curve.js
@@ -0,0 +1,116 @@
+import _ from 'lodash';
+
+import { getFractalDispacementForPoint } from './fractal';
+import {
+  getPixelColorAtXY,
+  isInColorThreshhold
+} from './color';
+
+export function renderCurves(svgSettings, curves) {
+  let renderString = '';
+  for (let i = 0; i < curves.length; i++) {
+    const { x, y, controlPoints, strokeWidth, strokeColor } = curves[i];
+
+    let curvePath = `M ${x} ${y} C`;
+    _.each(controlPoints, (point, index) => {
+      curvePath += ` ${point.x} ${point.y}` + (index === controlPoints.length - 1 ? '' : ',');
+    });
+
+    renderString += `<path d="${curvePath}" style="stroke: ${strokeColor}; stroke-width: ${strokeWidth}; fill: none;" />`;
+  }
+
+  return renderString;
+}
+
+function createCurveAtPoint(baseX, baseY, settings) {
+  const {
+    amplitude,
+    amplitudeRandomness,
+    applyFractalDisplacement,
+    direction,
+    directionRandomness,
+    displaceOrigin,
+    strokeColor,
+    strokeWidth,
+    strokeWidthRandomness,
+    wavelength,
+    wavelengthRandomness,
+    waves,
+    wavesRandomness
+  } = settings;
+
+  let x = baseX;
+  let y = baseY;
+
+  if (applyFractalDisplacement) {
+    const { xDisplacement, yDisplacement } = getFractalDispacementForPoint(baseX, baseY, settings);
+
+    x += xDisplacement;
+    y += yDisplacement;
+  }
+
+  const dir = (-direction) + 180 * directionRandomness * Math.random();
+  const xDir = Math.cos(dir * (Math.PI / 180));
+  const yDir = Math.sin(dir * (Math.PI / 180));
+  const inverseXDir = Math.cos((dir - 90) * (Math.PI / 180));
+  const inverseYDir = Math.sin((dir - 90) * (Math.PI / 180));
+  const wavelen = wavelength * (1 - Math.random() * wavelengthRandomness);
+  const amp = amplitude * (1 - Math.random() * amplitudeRandomness);
+  const wavAmount = Math.round(waves * (1 - Math.random() * wavesRandomness));
+  const controlPoints = [];
+  for (let i = 0; i < wavAmount; i++) {
+    controlPoints.push({
+      x: x + (((i * wavelen) + wavelen / 4) * xDir + inverseXDir * amp),
+      y: y + (((i * wavelen) + wavelen / 4) * yDir + inverseYDir * amp)
+    }, {
+      x: x + (((i * wavelen) + wavelen * (3 / 4)) * xDir - inverseXDir * amp),
+      y: y + (((i * wavelen) + wavelen * (3 / 4)) * yDir - inverseYDir * amp)
+    }, {
+      x: x + (((i + 1) * wavelen) * xDir),
+      y: y + (((i + 1) * wavelen) * yDir)
+    });
+
+    if (displaceOrigin) {
+      for (let k = controlPoints.length - 3; k < controlPoints.length; k++) {
+        const {
+          xDisplacement,
+          yDisplacement
+        } = getFractalDispacementForPoint(controlPoints[k].x, controlPoints[k].y, settings);
+
+        controlPoints[k].x += xDisplacement;
+        controlPoints[k].y += yDisplacement;
+      }
+    }
+  }
+
+  const curve = { x, y,
+    controlPoints,
+    strokeColor,
+    strokeWidth: strokeWidth * (1 - Math.random() * strokeWidthRandomness)
+  };
+
+  return curve;
+}
+
+export function createCurves(settings, imageData, width, height) {
+  const {
+    renderEveryXPixels,
+    renderEveryYPixels
+  } = settings;
+
+  const curves = [];
+
+  for (let x = 0; x < width; x += renderEveryXPixels) {
+    for (let y = 0; y < height; y += renderEveryYPixels) {
+      const pixelColor = getPixelColorAtXY(imageData, x, y, width);
+      if (!isInColorThreshhold(pixelColor, settings)) {
+        continue;
+      }
+
+      curves.push(createCurveAtPoint(x, y, settings, pixelColor));
+    }
+  }
+
+  return curves;
+}
+
--- src/svg-renderer/svg-renderer.js
+++ src/svg-renderer/svg-renderer.js
@@ -1,109 +1,7 @@
-import noise from '../utils/noise';
-
 import { SVG_RENDER_TYPES } from '../controller/Controller';
 import { createCircles, renderCircles } from './circle';
+import { createCurves, renderCurves } from './curve';
 import { createLines, renderLines } from './line';
-
-// How to fill - recursive filling functions.
-
-function isPixelInGridThreshold(pixel, svgSettings) {
-  const {
-    renderEveryXPixels,
-    renderEveryYPixels
-  } = svgSettings;
-
-  return pixel.x % renderEveryXPixels === 0 && pixel.y % renderEveryYPixels === 0;
-}
-
-function isPixelInColorThreshhold(pixel, controls) {
-  const {
-    minColorRecognized,
-    maxColorRecognized
-  } = controls;
-
-  return pixel.r >= minColorRecognized &&
-         pixel.g >= minColorRecognized &&
-         pixel.b >= minColorRecognized &&
-         pixel.r <= maxColorRecognized &&
-         pixel.g <= maxColorRecognized &&
-         pixel.b <= maxColorRecognized;
-}
-
-function applyFractalFieldToPoint(pixel, {
-  applyFractalDisplacement,
-  displacementAmount,
-  fractalRatioX,
-  fractalRatioY,
-  fractalRandomSeed
-}) {
-  if (!applyFractalDisplacement) {
-    return;
-  }
-
-  noise.seed(fractalRandomSeed);
-
-  const angleNoiseValue = Math.abs(noise.simplex2(pixel.x * fractalRatioX, pixel.y * fractalRatioY)) * Math.PI * 2;
-  const displacementNoiseValue = Math.abs(noise.simplex2((pixel.x * fractalRatioX) + 20000, (pixel.y * fractalRatioY) + 20000)) * displacementAmount;
-
-  pixel.x += Math.cos(angleNoiseValue) * displacementNoiseValue;
-  pixel.y += Math.sin(angleNoiseValue) * displacementNoiseValue;
-}
-
-function tryToRenderPixel(pixel, svgSettings) {
-  // eslint-disable-next-line default-case
-  switch (svgSettings.svgRenderType) {
-    case SVG_RENDER_TYPES.CURVE: {
-      if (isPixelInGridThreshold(pixel, svgSettings) &&
-          isPixelInColorThreshhold(pixel, svgSettings)) {
-        const {
-          amplitude,
-          amplitudeRandomness,
-          direction,
-          directionRandomness,
-          strokeWidth,
-          strokeWidthRandomness,
-          wavelength,
-          wavelengthRandomness,
-          waves,
-          wavesRandomness
-        } = svgSettings;
-
-        // TODO(@Rhys): Avoid mutating functions.
-        applyFractalFieldToPoint(pixel, svgSettings);
-
-        const displayColor = 'rgb(28, 32, 38)';
-        // While through wavelength to end.
-        let curvePath = `M ${pixel.x} ${pixel.y}`;
-        const dir = (-direction) + 180 * directionRandomness * Math.random();
-        const xDir = Math.cos(dir * (Math.PI / 180));
-        const yDir = Math.sin(dir * (Math.PI / 180));
-        const inverseXDir = Math.cos((dir - 90) * (Math.PI / 180));
-        const inverseYDir = Math.sin((dir - 90) * (Math.PI / 180));
-        const wavelen = wavelength * (1 - Math.random() * wavelengthRandomness);
-        const amp = amplitude * (1 - Math.random() * amplitudeRandomness);
-        const wavAmount = Math.round(waves * (1 - Math.random() * wavesRandomness));
-        for (let i = 0; i < wavAmount; i++) {
-          let points = [{
-            x: pixel.x + (((i * wavelen) + wavelen / 4) * xDir + inverseXDir * amp),
-            y: pixel.y + (((i * wavelen) + wavelen / 4) * yDir + inverseYDir * amp)
-          }, {
-            x: pixel.x + (((i * wavelen) + wavelen * (3 / 4)) * xDir - inverseXDir * amp),
-            y: pixel.y + (((i * wavelen) + wavelen * (3 / 4)) * yDir - inverseYDir * amp)
-          }, {
-            x: pixel.x + (((i + 1) * wavelen) * xDir),
-            y: pixel.y + (((i + 1) * wavelen) * yDir)
-          }];
-
-          curvePath += ` C ${points[0].x} ${points[0].y}, ${points[1].x} ${points[1].y}, ${points[2].x} ${points[2].y}`;
-        }
-
-        const strokeW = strokeWidth * (1 - Math.random() * strokeWidthRandomness);
-        return `<path d="${curvePath}" style="stroke: ${displayColor}; stroke-width: ${strokeW}; fill: none;" />`;
-      }
-      break;
-    }
-  }
-}
 
 export function renderSvgString(imageData, svgSettings, width, height, done) {
   setImmediate(() => {
@@ -120,30 +18,17 @@ export function renderSvgString(imageData, svgSettings, width, height, done) {
         svgString += renderCircles(svgSettings, circles);
         break;
       }
+      case SVG_RENDER_TYPES.CURVE: {
+        const curves = createCurves(svgSettings, imageData, width, height);
+
+        svgString += renderCurves(svgSettings, curves);
+        break;
+      }
       case SVG_RENDER_TYPES.LINE: {
         const lines = createLines(svgSettings, imageData, width, height);
 
         svgString += renderLines(svgSettings, lines);
         break;
-      }
-    }
-
-    if (svgSettings.svgRenderType === SVG_RENDER_TYPES.CURVE) {
-      for (let i = 0; i < imageData.length / 4; i++) {
-        const pixelIndex = i * 4;
-        const pixel = {
-          x: i % width,
-          y: Math.floor(i / width),
-          r: imageData[pixelIndex],
-          g: imageData[pixelIndex + 1],
-          b: imageData[pixelIndex + 2],
-          a: imageData[pixelIndex + 3] / 255
-        };
-
-        const svgPixelString = tryToRenderPixel(pixel, svgSettings);
-        if (svgPixelString) {
-          svgString += svgPixelString;
-        }
       }
     }
 
