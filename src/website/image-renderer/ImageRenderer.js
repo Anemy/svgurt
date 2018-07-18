be42110d679b4d25bc524324dbda0a92edfc252d
Rhys Howell <Anemy@users.noreply.github.com>
2018-07-18 13:47:06 -0400

    📱 (#11)

--- src/website/image-renderer/ImageRenderer.js
+++ src/website/image-renderer/ImageRenderer.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import './ImageRenderer.css';
 
@@ -10,6 +11,7 @@ import { updateGuiDisplay, updateRenderType } from '../controller/Controller';
 import { manipulateImageData } from '../../core/image-manipulator';
 import { renderSvgString } from '../../core/svg-renderer/svg-renderer';
 import { downloadSVGString } from './downloader';
+import SvgurtTextLogo from '../home/SvgurtTextLogo';
 
 export default class ImageRenderer extends Component {
   constructor(props) {
@@ -227,6 +229,29 @@ export default class ImageRenderer extends Component {
 
     return (
       <div className="svgee-image-renderer">
+        <div className="svgee-top-nav-bar">
+          <SvgurtTextLogo />
+          <Link
+            className="svgee-top-nav-link"
+            to="/"
+          >
+            Home
+          </Link>
+          <Link
+            className="svgee-top-nav-link"
+            to="cli"
+          >
+            Cli Docs
+          </Link>
+          <a
+            className="svgee-top-nav-link"
+            href="https://github.com/Anemy/svgurt"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Github
+          </a>
+        </div>
         <ControlBar
           currentConfigName={currentConfigName}
           configNames={configNames}
@@ -247,7 +272,7 @@ export default class ImageRenderer extends Component {
         />
         {loadingImage && <p>Loading Image...</p>}
         {isRendering && <p>Building Image...</p>}
-        <div className="grid no-gutters">
+        <div className="svgee-image-showing-window grid no-gutters">
           <div className="unit half">
             <div className="svgee-home-item">
               <canvas
