9fbf247bb5c6a6fac209fb7eb488fc393697ce62
unknown <rhysh@live.com>
2022-01-16 03:23:08 -0500

    dependency upgrades and github link on landing

--- src/website/landing-info/LandingInfo.js
+++ src/website/landing-info/LandingInfo.js
@@ -15,6 +15,12 @@ export default class LandingInfo extends Component {
           onClick={closeLandingInfo}
         />
         <div className="svgee-landing-page">
+          <a
+            href="https://github.com/anemy/svgurt"
+            className="svgee-github-link"
+          >
+            <img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149" className="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1" />
+          </a>
           <img
             className="svgee-logo-image"
             src="/images/svgurt_logo.png"
